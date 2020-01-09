c36f054c5d004ee1b8f72faa5b89149a140dc18f
Matt LeFort <matthewlefort@gmail.com>
2020-01-09 14:11:31 -0800

    Coursera WD4E CSS W1 + TM HTML-CSS Playlist mobile first design

---
+++ TravisMedia_WebDevCrashCourses/HTML_CSS_Playlist/ResponsiveMobileFirstWebsite/js/main.js
@@ -0,0 +1,18 @@
+// Animate smooth scroll
+$('#view-work').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
